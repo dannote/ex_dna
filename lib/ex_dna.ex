96483e239bd1f63b2c93c28f6229d3fed3afc552
Danila Poyarkov <dev@dannote.net>
2026-03-06 22:12:11 +0300

    Initial mix project

---
+++ lib/ex_dna.ex
@@ -0,0 +1,18 @@
+defmodule ExDNA do
+  @moduledoc """
+  Documentation for `ExDNA`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExDNA.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_dna_test.exs
@@ -0,0 +1,8 @@
+defmodule ExDNATest do
+  use ExUnit.Case
+  doctest ExDNA
+
+  test "greets the world" do
+    assert ExDNA.hello() == :world
+  end
+end
